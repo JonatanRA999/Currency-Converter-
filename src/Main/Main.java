a784a63f3174c72ce0734cba5d6900e1ccb01b34
JonatanR <jonatanjr-2017@outlook.es>
2024-07-03 15:20:12 -0500

    Se sube imagen de evidencia

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,30 +1,34 @@
 package Main;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
-
         try {
             HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest
-                    .newBuilder()
+            HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create("https://v6.exchangerate-api.com/v6/49d66aedba4681bd5afdcccd/latest/USD"))
                     .build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse response = client.send(request , HttpResponse.BodyHandlers.ofString());
 
             System.out.println(response.body());
-
-        }catch (Exception e){
-            System.out.println("Exception of : "+e.getMessage());
+        }catch (Exception e)
+        {
+            System.out.println("Mensaje : "+ e.getMessage());
         }
     }
 }
